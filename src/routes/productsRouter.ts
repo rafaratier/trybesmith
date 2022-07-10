8fc2abef933d7c2adfc887bee36e78acb1f0fa7d
Rafael <rafaelratier@hotmail.com>
2022-07-10 15:05:09 -0300

    feat(productsrouter): add route to create new product

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -4,5 +4,6 @@ import productsController from '../controllers/productsController';
 const productsRouter = express.Router();
 
 productsRouter.get('/', productsController.getAllProducts);
+productsRouter.post('/', productsController.createProduct);
 
 export default productsRouter;
